4208543d7792eec566f56634e6683b26df11e0b7
Blanca Chavarria <bchavarria@utch.edu.mx>
2024-02-08 17:45:10 -0600

    Primer commit PWA

---
+++ main.js
@@ -0,0 +1,13 @@
+//Cargar Service Worker
+if('serviceWorker' in navigator) 
+{
+    console.log("Puedes usar el Service Worker");
+    //configuración del SW
+    navigator.serviceWorker.register('./sw.js')
+                            .then(res=>console.log('SW cargado correctamente',res))
+                            .catch(err => console.log('service Worker no se ha podido registrar',err));
+}   
+else
+{
+    console.log("No se puede usar el Service Worker");
+}
